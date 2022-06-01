5a5695ef0d60c5a157ec1f33f9a27bd010c8f92e
Jakob Voss <voss@gbv.de>
2022-06-01 18:29:44 +0200

    Introduce format families

--- test/model.js
+++ test/model.js
@@ -19,45 +19,57 @@ describe("Range", () => {
 })
 
 describe("FieldIdentifier", () => {
-  const { valid, invalid } = jsonFile("./files/field-identifiers.json")
+  jsonFile("./files/field-identifiers.json").forEach(({ family, valid, invalid }) => {
 
-  it("parses valid field identifiers", () => {
+    it(`parses valid ${family} field identifiers`, () => {
     /* eslint-disable no-unused-vars */
-    Object.entries(valid).forEach(([id, {match, nomatch, ...result}]) => {
-      expect(new FieldIdentifier(id)).deep.equal(result)
+      Object.entries(valid).forEach(([id, {match, nomatch, ...result}]) => {
+        expect(new FieldIdentifier(id, family)).deep.equal(result)
+      })
     })
-  })
 
-  it("detects invalid field identifiers", () => {
-    invalid.forEach(id => {
-      expect(() => new FieldIdentifier(id)).to.throw()
+    it(`detects invalid ${family} field identifiers`, () => {
+      invalid.forEach(id => {
+        expect(() => new FieldIdentifier(id, family))
+          .to.throw(`Invalid ${family} field identifier '${id}`)
+      })
     })
-  })
 
-  it("matches fields", () => {
-    Object.entries(valid).forEach(([id, {match,nomatch}]) => {
-      const identifier = new FieldIdentifier(id)
-      if (match) {
-        match.forEach(value => expect(identifier.match(value)).ok)
-      }
-      if (nomatch) {
-        nomatch.forEach(value => expect(id.match(value)).not.ok)
-      }
+    it(`matches ${family} fields`, () => {
+      Object.entries(valid).forEach(([id, {match,nomatch}]) => {
+        const identifier = new FieldIdentifier(id, family)
+        if (match) {
+          match.forEach(value => expect(identifier.match(value)).ok)
+        }
+        if (nomatch) {
+          nomatch.forEach(value => expect(id.match(value)).not.ok)
+        }
+      })
+    })
+
+    it("ignores occurrence /00 in pica family", () => {
+      expect(new FieldIdentifier("123X/00", "pica")).deep.equal({tag: "123X"})
+    })
+
+    it("rejects unknown format family", () => {
+      expect(() => new FieldIdentifier("x", "?!"))
+        .to.throw("Format family '?!' not supported.")
     })
   })
 })
 
-describe("FieldSchedule", () => {
-  const ids = ["X", "012X/03", "000X/00-02", "234Xx1" ].sort()
-  const schedule = new FieldSchedule(Object.fromEntries(ids.map(id => [id, {label: id}])))
+describe("FieldSchedule (pica)", () => {
+  const ids = ["003@", "012X/03", "000X/00-02", "234Xx1" ].sort()
+  const fields = Object.fromEntries(ids.map(id => [id, {label: id}]))
+  const schedule = new FieldSchedule({ fields, family: "pica" })
 
   it("acts like an object", () => {
     expect(Object.keys(schedule)).deep.equal(ids)
   })
 
   it("contains fields", () => {
-    expect("X" in schedule).ok
-    expect(schedule["X"]).deep.equal({label: "X"})
+    expect("003@" in schedule).ok
+    expect(schedule["003@"]).deep.equal({label: "003@"})
     expect("identifier" in schedule).not.ok
     expect(schedule.identifier({ tag: "012X", occurrence: "03" })).equal("012X/03")
     expect(schedule.identifier({ tag: "000X", occurrence: "02" })).equal("000X/00-02")
