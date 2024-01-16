727ed952f1ed680e444c294e12f6b1cae078c6f2
Jakob Voss <voss@gbv.de>
2024-01-16 12:13:44 +0100

    Move gh-pages directory

--- bin/pages.sh
+++ bin/pages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-cp test/schemas/marc21-bibliographic.json pages
-#cp test/schemas/k10plus-zentral-marc.json pages
-#curl -s 'https://format.k10plus.de/avram.pl?profile=k10plus' > pages/k10plus-pica.json
+cp test/schemas/marc21-bibliographic.json docs
+#cp test/schemas/k10plus-zentral-marc.json docs
+#curl -s 'https://format.k10plus.de/avram.pl?profile=k10plus' > docs/k10plus-pica.json
 
-cd pages
+cd docs
 for name in k10plus-pica marc21-bibliographic k10plus-zentral-marc unimarc; do
   ../bin/avram.js -n -d $name.json > $name.html
 done
